33b92cd5020951a58faa3ff69115b0c0738f69a7
Nisha Shah <nisha20.shah@gmail.com>
2019-09-21 14:41:35 -0700

    Sample project setup with TypeScript

---
+++ TypeScriptDemo/scripts/app.js
@@ -0,0 +1,6 @@
+function sayHello() {
+    var compiler = document.getElementById("compiler").value;
+    var framework = document.getElementById("framework").value;
+    return 'Hi from ${compiler} and ${framework}!';
+}
+//# sourceMappingURL=app.js.map
---
+++ TypeScriptDemo/scripts/app.ts
@@ -0,0 +1,5 @@
+﻿function sayHello() {
+    const compiler = (document.getElementById("compiler") as HTMLInputElement).value;
+    const framework = (document.getElementById("framework") as HTMLInputElement).value;
+    return 'Hi from ${compiler} and ${framework}!';
+}
